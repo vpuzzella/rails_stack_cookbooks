568eee55285aa4b3a78a93f554e86cf19c6549e6
vpuzzella <vincepuz@gmail.com>
2011-11-23 19:34:26 -0500

    First commit

---
+++ rails_stack/recipes/default.rb
@@ -0,0 +1,68 @@
+# Add Dotdeb to apt sources
+[ 'deb http://packages.dotdeb.org stable all',
+  'deb-src http://packages.dotdeb.org stable all'].each do |line|
+  execute "Add Dotdeb apt source: #{line}" do
+    command %[echo '#{line}' >> /etc/apt/sources.list]
+    not_if "cat /etc/apt/sources.list | grep '#{line}'"
+  end
+end
+execute "wget -q -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -"
+execute 'apt-get update' do
+  returns [0,
+    100 #Release file expired, ignoring
+  ]
+end
+
+include_recipe 'postfix'
+
+package 'redis-server'
+
+package 'memcached'
+
+include_recipe "postgresql::server"
+
+gem_package 'bundler'
+execute 'bundle install' do
+  cwd '/vagrant'
+end
+# Snippet from opscode to reload gems
+require 'rubygems'
+Gem.clear_paths
+
+# Create a database user based on env and db config
+require 'yaml'
+
+rails_env = node[:rails_env] || 'development'
+user_name = YAML::load(File.open '/vagrant/config/database.yml')[rails_env]['username']
+execute "create database user: #{user_name}" do
+  user 'postgres'
+  not_if "psql -c '\\du #{user_name}' | grep #{user_name}", :user => 'postgres'
+  #TODO: Base createuser options on rails_env. IMO, --superuser is acceptable for development/test only
+  command "createuser --superuser #{user_name}"
+end
+
+gem_package 'unicorn'
+directory '/etc/unicorn'
+template "/etc/unicorn/unicorn.rb" do
+  source "unicorn.rb.erb"
+end
+template "/etc/init.d/unicorn" do
+  source "unicorn.init.erb"
+  mode 0774
+end
+service "unicorn" do
+  supports :restart => true
+  action :enable
+  subscribes :restart, resources(:template => ["/etc/init.d/unicorn", "/etc/unicorn/unicorn.rb"])
+end
+
+package 'nginx'
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+end
+service "nginx" do
+  supports :restart => true
+  action :enable
+  subscribes :restart, resources(:template => ["/etc/nginx/nginx.conf"])
+end
+
